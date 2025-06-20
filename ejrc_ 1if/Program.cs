bc329f759e77ef54a68335ffdb2a2ac86b1cec3a
Denis <74616294@idexperujapon.edu.pe>
2025-06-20 16:16:19 -0500

    Agregar archivos de proyecto.

---
+++ ejrc_ 1if/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejrc__1if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.WriteLine("introduce un numero entero: "); 
+            n1 = Convert.ToInt32(Console.ReadLine());
+            if (n1>0)
+            {
+                Console.WriteLine("El numero es positivo");
+            }
+        }
+    }
+}
